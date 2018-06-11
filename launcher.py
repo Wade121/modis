33f2a20d3e2e5c53d197c74f305911f2c18f693e
Wade121 <Wadesmith201@hotmail.com>
2018-06-11 17:26:08 +1000

    Create launcher.py

---
+++ launcher.py
@@ -0,0 +1,9 @@
+import modis
+
+DISCORD_TOKEN = "NDU1NjIyMDMwNzUxMDM5NTAw.Df-qnQ.r9g5_akgARlFhy5t6-2YIu5WIOQ"
+CLIENT_ID = "455622030751039500"
+
+modis.gui(
+    discord_token=DISCORD_TOKEN,
+    discord_client_id=CLIENT_ID
+)
